e913d00ac67746dff2fe9349110f6ea79acfd9d4
TomasBkk <pr0digy1@hotmail.com>
2023-09-11 18:47:48 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,17 @@
 import * as actions from '@actions/core';
 import * as github from '@actions/github';
-import * as exec from '@actions/exec';
-
 
 async function run() {
 	try {
 		actions.info('Fetching release notes...');
 
 		const token = actions.getInput('token', { required: true });
-		//const tagName = actions.getInput('tag-name', { required: true });
-		//const prevTagName = actions.getInput('prev-tag-name', { required: false });
+		const tagName = actions.getInput('tag-name', { required: true });
+		const prevTagName = actions.getInput('prev-tag-name', { required: false });
 		const branch = actions.getInput('branch', { required: false }) || 'main';
 		if (!token) throw new Error('Input "token" is required');
-		//if (!tagName) throw new Error('Input "tag-name" is required');
+		if (!tagName) throw new Error('Input "tag-name" is required');
 		if (!branch) throw new Error('Input "branch" is required');
-
-        // Use 'exec' to run the 'git' command and capture its output
-        let tagNames = '';
-        await exec.exec('git', ['for-each-ref', '--count=2', '--sort=-creatordate', '--format', '%(refname:short)', 'refs/tags'], {
-            silent: false, // Suppress output to the console
-            listeners: {
-                stdout: (data: Buffer) => {
-                    tagNames += data.toString();
-                },
-            },
-        });
-
-        actions.info('Tag names retrieved: ' + tagNames);
-
-        tagNames = tagNames.trim();
-        const tagNamesArray = tagNames.split('\n');
-
-        if (tagNamesArray.length < 2) {
-            throw new Error('Not enough tags found to generate release notes');
-        }
-
-        const tagName = tagNames[0]; // Last tag name
-        const prevTagName = tagNames[1]; // Second-to-last tag name
 
 		const octokit = github.getOctokit(token);
 
